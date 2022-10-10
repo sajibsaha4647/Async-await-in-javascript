c6d7b3326de2a2b9b2ac93a01856451022842e09
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-10 14:16:45 +0600

    javascript async await

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-//async and await in javascript
+// //async and await in javascript
+
+// let newpromise = new Promise((res, rej) => {
+//   setTimeout(res, 500, "one2");
+// });
+
+// getdata = async () => {
+//   let promis = new Promise((res, reject) => {
+//     setTimeout(res, 100, "one");
+//   });
+
+//   let s = await promis;
+//   console.log(s);
+// };
+
+// getdata();
+
+// //resolve here
+// // newpromise.then((res) => {
+// //   console.log(res);
+// // });
+
+// Promise.race([newpromise2, newpromise]).then((res) => {
+//   console.log(res);
+// });
+
+// const firstPromise = new Promise((res, rej) => {
+//   setTimeout(res, 500, "one");
+// });
+// const secondPromise = new Promise((res, rej) => {
+//   setTimeout(res, 100, "two");
+// });
+
+// Promise.race([firstPromise, secondPromise]).then((res) => {
+//   console.log(res);
+// });
+
+async function getapidata() {
+  let data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  //   console.log(JSON.stringify(data));
+
+  data = await data.json();
+  console.log(data);
+}
+
+// getapidata();
+
+async function onlyasync() {
+  return "bdtask";
+}
+
+console.log(onlyasync());
